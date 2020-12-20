c27d565acdcb84bd35207ed6eaed6f479ba3baf4
MaksMacioszczyk <>
2020-12-20 20:01:06 +0100

    -Poprawienie błędu

--- GrzybobranieTheGame2D/main.cpp
+++ GrzybobranieTheGame2D/main.cpp
@@ -6,6 +6,7 @@
 #include "WorldGen.h"
 #include "Atlas.h"
 #include "Zakonczenie.h"
+#include <string>
 
 using namespace sf;
 using namespace std;
@@ -16,7 +17,25 @@ int main()
     float playerSpeed = 0.05f;
     bool isMenuOpen = true, isPlaying = false, isAtlasOpen = false, isZakonczenieOpen = false;
     int punkty = 0;
+
+    Font font;
+    if (!font.loadFromFile("arial.ttf"))
+    {
+
+    }
+
+    Text punkty_tekst;
+    punkty_tekst.setFont(font);
+    punkty_tekst.setFillColor(Color(58, 182, 189));
+    punkty_tekst.setString("Punkty: " + to_string(punkty)+" / "+to_string(numOfWinningPoints));
+    punkty_tekst.setPosition(Vector2f(30,30));
     
+    SoundBuffer winSound;
+    winSound.loadFromFile("winSound.wav");
+
+    SoundBuffer pickup_sound;
+    pickup_sound.loadFromFile("pickup_sound.wav");
+
     SoundBuffer menuMusic;
     menuMusic.loadFromFile("menu.wav");
 
@@ -30,15 +49,12 @@ int main()
     Texture sky;
     sky.loadFromFile("sky.jpg");
 
-    Atlas atlas(window.getSize().x, window.getSize().y);
+    Atlas atlas(window.getSize().x, window.getSize().y, &sky);
     Menu menu(window.getSize().x, window.getSize().y,&sky);
-    Zakonczenie zakonczenie(window.getSize().x, window.getSize().y);
+    Zakonczenie zakonczenie(window.getSize().x, window.getSize().y, &sky);
 
     Texture playerTexture;
     playerTexture.loadFromFile("Male 14-1.png");
-
-
-    
 
     Texture background_texture;
     background_texture.loadFromFile("grassTexture.jpg",IntRect(0,0,800,600));
@@ -55,8 +71,10 @@ int main()
     Clock clock;
     
     Sound sound;
+    sound.setVolume(2);
     sound.setBuffer(menuMusic);
     sound.play();
+    Sound backSound;
 
 
     while (window.isOpen())
@@ -85,8 +103,7 @@ int main()
                         case 0:
                             if(isPlaying == false)
                             {
-                                sound.setBuffer(gameMusic);
-                                sound.play();
+                                
                                 break;
                             }
                             cout << "Play button" << endl;
@@ -98,6 +115,8 @@ int main()
                             cout << "New game button" << endl;
                             player.resetPlayer(Vector2f(20,20));
                             isPlaying = true;
+                            punkty = 0;
+                            punkty_tekst.setString("Punkty: " + to_string(punkty) + " / " + to_string(numOfWinningPoints));
                             sound.setBuffer(gameMusic);
                             sound.play();
                             world.generateTrees(window.getSize().x, window.getSize().y);
@@ -132,6 +151,8 @@ int main()
                         isMenuOpen = true;
                         isPlaying = false;
                         punkty = 0;
+                        sound.setBuffer(menuMusic);
+                        sound.play();
                         break;
                     }
                 }
@@ -148,11 +169,18 @@ int main()
                         if (world.isIntersecting(player))
                         {
                             punkty += 10;
+                            punkty_tekst.setString("Punkty: " + to_string(punkty) + " / " + to_string(numOfWinningPoints));
+                            backSound.setBuffer(pickup_sound);
+                            backSound.play();
+
                             cout << "Punkty: " << punkty << endl;
                         }
                         if (punkty == numOfWinningPoints)
                         {
+                            
                             isZakonczenieOpen = true;
+                            backSound.setBuffer(winSound);
+                            backSound.play();
                         }
                         break;
                     case Keyboard::P:
@@ -197,7 +225,7 @@ int main()
             player.Update(deltaTime);       
             player.Draw(window);
             world.Draw(window);
-           
+            window.draw(punkty_tekst);
         }
 
         
